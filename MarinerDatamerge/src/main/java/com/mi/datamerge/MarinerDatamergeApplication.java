1b84c5dc1283b58a7ddd844e66ff3a3e9ca099f4
Alex Kayser <akctba@gmail.com>
2021-08-18 18:48:36 -0400

    Project creation

---
+++ MarinerDatamerge/src/main/java/com/mi/datamerge/MarinerDatamergeApplication.java
@@ -0,0 +1,13 @@
+package com.mi.datamerge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarinerDatamergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarinerDatamergeApplication.class, args);
+	}
+
+}
